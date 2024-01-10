169a5283d38707965799c4fb3a9fb319bb7fef47
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-01-10 20:03:57 +0200

    add people in projects become in number

--- constructions/serializers.py
+++ constructions/serializers.py
@@ -3,12 +3,15 @@ from .models import Project,ProjectMember
 
 class ProjectMembersSerializers(serializers.ModelSerializer):
     member_name = serializers.SerializerMethodField()
+    phone_number = serializers.SerializerMethodField()
     class Meta:
         model = ProjectMember
-        fields = ['id','project','member','member_name']
+        fields = ['id','project','member','member_name','phone_number']
 
     def get_member_name(self, obj):
         return obj.member.first_name if obj.member.first_name else 'No Name'
+    def get_phone_number(self, obj):
+        return str(obj.member.phone_number)
 class ProjectSerializers(serializers.ModelSerializer):
     project_owner = serializers.CharField(max_length=255,read_only=True)
     members = ProjectMembersSerializers(many=True, source='projectmember_set', read_only=True)
