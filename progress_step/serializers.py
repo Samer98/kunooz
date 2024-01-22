b1ba891ed1d402adaf8822f5deabe28cdccd33a2
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-01-22 17:20:47 +0200

    adding profile piccture to login

--- progress_step/serializers.py
+++ progress_step/serializers.py
@@ -2,29 +2,41 @@ from rest_framework import serializers
 from .models import ProgressStep, ProgressStepComment
 
 
-
 class ProgressStepSerializers(serializers.ModelSerializer):
     order = serializers.IntegerField(read_only=True)
     sub_steps = serializers.SerializerMethodField(read_only=True)
+
+    # sub_steps_comments = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = ProgressStep
-        fields = ['id','title','project',"parent","order","is_finished",'sub_steps']
+        fields = ['id', 'title', 'project', "parent", "order", "is_finished", 'sub_steps']
 
     def get_sub_steps(self, obj):
         sub_steps = ProgressStep.objects.filter(parent=obj)
         if sub_steps.exists():
-            serializer = ProgressStepSerializers(sub_steps, many=True)
+            serializer = ProgressStepSerializers(sub_steps, many=True, context=self.context)
+
+            # Check if there are comments for each sub-step
+            for sub_step_data in serializer.data:
+                sub_step_instance = ProgressStep.objects.get(id=sub_step_data['id'])
+                comments = ProgressStepComment.objects.filter(sub_step=sub_step_instance)
+                if comments.exists():
+                    comment_serializer = ProgressStepCommentSerializers(comments, many=True)
+                    sub_step_data['sub_steps_comments'] = comment_serializer.data
+                else:
+                    sub_step_data['sub_steps_comments'] = None
+
             return serializer.data
         return None
-
 
 
 class ProgressStepCommentSerializers(serializers.ModelSerializer):
     date_created = serializers.DateField(read_only=True)
     user = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = ProgressStepComment
-        fields = ['id','user','sub_step','file','comment','date_created']
+        fields = ['id', 'user', 'sub_step', 'file', 'comment', 'date_created']
 
     def get_user(self, obj):
         if obj.user:
