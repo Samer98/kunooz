e68e24a4ad691acea2486b7000a9d999a0e36f0f
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-01-24 18:16:21 +0200

    adding notifcations

--- notifcations/models.py
+++ notifcations/models.py
@@ -7,6 +7,7 @@ class Notification(models.Model):
     type = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    extra_data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return str(self.user) + " | " +str(self.message)
