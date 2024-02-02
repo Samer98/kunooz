953976fe2d78a2ec8cd31c1d6f0f63ea78ac26d9
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-02-02 13:53:17 +0200

    adding media

--- kunooz/urls.py
+++ kunooz/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
 from members.views import CustomTokenObtainPairView
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,4 +37,4 @@ urlpatterns = [
     path('notifcations/', include('notifcations.urls')),
     path('pricing_tender/', include('pricing_tender.urls')),
 
-]
+] + static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
