9ed689dfa8b9462e093cacdb264c47475c8ce6c1
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-02-04 12:42:09 +0200

    fixing pagination

--- approval/urls.py
+++ approval/urls.py
@@ -8,4 +8,7 @@ router.register('Approval', views.ApprovalViewSet,basename="Approval")
 router.register('ApprovalComment', views.ApprovalCommentViewSet,basename="ApprovalComment")
 
 urlpatterns = [
+                  path('projects/<int:project_id>/record/<int:pk>/',
+                       views.ApprovalViewSet.as_view({'get': 'record_info', 'put': 'record_info'}),
+                       name='report-record-info'),
               ] + router.urls
