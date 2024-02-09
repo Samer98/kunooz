e5ac7f3e2e32a938680b358a6c7df5f96830de6f
Samer98 <63647160+Samer98@users.noreply.github.com>
2024-02-09 19:18:54 +0200

    add users info in notifcation

--- notifcations/serializer.py
+++ notifcations/serializer.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Notification
         fields = ['id', 'user','type', 'message','extra_data', 'created_at', 'is_read']
+
+    def get_user(self, obj):
+        if obj.user:
+            user_data = {
+                'id': obj.user.id,
+                'first_name': obj.user.first_name,
+                'second_name': obj.user.second_name,
+                'profile_picture': obj.user.profile_picture.url if obj.user.profile_picture else None,
+                'role': str(obj.user.role),
+                'phone_number': str(obj.user.phone_number)
+            }
+            return user_data
+        return None
